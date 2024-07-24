d4a1d0df1ecb9a131a22789d01e806468e36d9de
Mateo <matferblan@gmail.com>
2024-07-24 18:57:49 +0100

    Deleted duplicate Automation

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -31,12 +31,6 @@ export class AppService {
       link: "automation",
       image: "assets/images/pic05.jpg",
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
-    },
-    {
-      title: "Automation",
-      link: "automation",
-      image: "assets/images/pic05.jpg",
-      snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     }
   ]
 
