59b0603bab013f92bc09c2c8a3dabd1085b23bbb
Rich Wincott <rich.wincott@gmail.com>
2024-07-24 22:37:33 +0100

    photos

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,26 +10,36 @@ export class AppService {
     {
       title: "Data Analytics",
       link: "data-analytics",
-      images: ["assets/images/DA Slide 4.png", "assets/images/DA Slide 4.png", "assets/images/DA Slide 4.png"],
+      images: ["assets/images/DA slide 1.png", "assets/images/DA slide 2.png", "assets/images/DA Slide 3.png", "assets/images/DA Slide 4.png"],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel felis hendrerit, sagittis lacus at, accumsan metus. Sed tempor, ligula a feugiat lobortis, odio arcu tristique nulla, nec egestas augue lorem a arcu. In ut quam eget libero mollis tincidunt finibus at eros. Cras porta lacus neque, eget efficitur mauris venenatis at. Aenean pellentesque dolor sit amet enim lacinia, quis mollis diam egestas. Fusce mattis, eros vel hendrerit venenatis, orci odio ultrices tellus, sed semper nunc quam sit amet eros. Quisque in ante lobortis, fringilla neque vel, finibus lacus. Nulla eget ante eros. Mauris sodales augue est, in tincidunt risus venenatis non. Nulla non lacinia metus. Aenean molestie placerat enim, id tristique felis auctor id. Sed vitae orci turpis. Donec consectetur odio ac mi vehicula, a consequat augue ornare."
     },
     {
       title: "Business Intelligence",
       link: "business-intelligence",
-      images: ["assets/images/altas mate-03.jpg", "assets/images/altas mate-03.jpg"],
+      images: ["assets/images/altas mate-01.jpg", "assets/images/altas mate-02.jpg", "assets/images/altas mate-03.jpg"],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     },
     {
       title: "Machine Learning",
       link: "machine-learning",
-      images: ["assets/images/pic04.jpg", "assets/images/pic04.jpg"],
+      images: [
+        "assets/images/pic01.jpg",
+        "assets/images/pic02.jpg",
+        "assets/images/pic03.jpg",
+        "assets/images/pic04.jpg",
+        "assets/images/pic05.jpg",
+        "assets/images/pic06.jpg",
+        "assets/images/pic07.jpg",
+        "assets/images/pic08.jpg",
+        "assets/images/pic09.jpg"
+      ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     },
     {
       title: "Automation",
       link: "automation",
-      images: ["assets/images/pic05.jpg"],
+      images: ["assets/images/Slide1.jpeg", "assets/images/Slide2.jpeg", "assets/images/Slide3.png"],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     }
   ]
