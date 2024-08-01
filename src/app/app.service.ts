be212027635d5592f4e3afcb82eda4a8cbea859b
Mateo <matferblan@gmail.com>
2024-08-01 11:49:47 +0100

    added new images and sorted each category

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,36 +10,51 @@ export class AppService {
     {
       title: "Data Analytics",
       link: "data-analytics",
-      images: ["assets/images/Slide2.png", "assets/images/Slide1.png", "assets/images/Slide3.png", "assets/images/Slide4.png", "assets/images/Slide5.png"],
+      images: [
+        "assets/images/Slide1.png", 
+        "assets/images/Slide2.png", 
+        "assets/images/Slide3.png", 
+        "assets/images/Slide4.png", 
+        "assets/images/Slide5.png"
+      ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel felis hendrerit, sagittis lacus at, accumsan metus. Sed tempor, ligula a feugiat lobortis, odio arcu tristique nulla, nec egestas augue lorem a arcu. In ut quam eget libero mollis tincidunt finibus at eros. Cras porta lacus neque, eget efficitur mauris venenatis at. Aenean pellentesque dolor sit amet enim lacinia, quis mollis diam egestas. Fusce mattis, eros vel hendrerit venenatis, orci odio ultrices tellus, sed semper nunc quam sit amet eros. Quisque in ante lobortis, fringilla neque vel, finibus lacus. Nulla eget ante eros. Mauris sodales augue est, in tincidunt risus venenatis non. Nulla non lacinia metus. Aenean molestie placerat enim, id tristique felis auctor id. Sed vitae orci turpis. Donec consectetur odio ac mi vehicula, a consequat augue ornare."
     },
     {
       title: "Business Intelligence",
       link: "business-intelligence",
-      images: ["assets/images/altas mate-03.jpg", "assets/images/altas mate-02.jpg", "assets/images/altas mate-01.jpg"],
+      images: [
+        "assets/images/PBI Slide1.png", 
+        "assets/images/PBI Slide2.png", 
+        "assets/images/PBI Slide3.png",
+        "assets/images/PBI Slide4.png",
+        "assets/images/PBI Slide5.png",
+        "assets/images/PBI Slide6.png"
+      ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     },
     {
       title: "Machine Learning",
       link: "machine-learning",
       images: [
-        "assets/images/pic01.jpg",
-        "assets/images/pic02.jpg",
-        "assets/images/pic03.jpg",
-        "assets/images/pic04.jpg",
-        "assets/images/pic05.jpg",
-        "assets/images/pic06.jpg",
-        "assets/images/pic07.jpg",
-        "assets/images/pic08.jpg",
-        "assets/images/pic09.jpg"
+        "assets/images/DS Slide3.png",
+        "assets/images/DS Slide6.png",
+        "assets/images/DS Slide7.png",
+        "assets/images/DS Slide4.png",
+        "assets/images/DS Slide2.png",
+        "assets/images/DS Slide5.png",
+        "assets/images/DS Slide1.png"
       ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     },
     {
       title: "Automation",
       link: "automation",
-      images: ["assets/images/Slide1.jpeg", "assets/images/Slide2.jpeg", "assets/images/Slide3.png"],
+      images: [
+        "assets/images/Slide1.jpeg", 
+        "assets/images/Slide2.jpeg", 
+        "assets/images/Slide3.png"
+      ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
     }
   ]
