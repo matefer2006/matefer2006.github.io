2617fcd6337ce4417af96d495900ca068419ac3f
Mateo <matferblan@gmail.com>
2024-08-01 11:52:16 +0100

    changed slide in DA section

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -28,7 +28,6 @@ export class AppService {
         "assets/images/PBI Slide2.png", 
         "assets/images/PBI Slide3.png",
         "assets/images/PBI Slide4.png",
-        "assets/images/PBI Slide5.png",
         "assets/images/PBI Slide6.png"
       ],
       snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam."
