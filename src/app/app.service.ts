98b4d85bb918c084b9856c1c442e92a5fc56d344
Mateo <matferblan@gmail.com>
2024-08-13 12:07:44 +0100

    changes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -17,7 +17,7 @@ export class AppService {
         "assets/images/Slide6.png", 
         "assets/images/Slide5.png"
       ],
-      snippet: "Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.",
+      snippet: "Transforming raw data into actionable insights using <strong>Python, SQL, and Excel</strong> to drive strategic decision-making and enhance business performance across various industries.",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel felis hendrerit, sagittis lacus at, accumsan metus. Sed tempor, ligula a feugiat lobortis, odio arcu tristique nulla, nec egestas augue lorem a arcu. In ut quam eget libero mollis tincidunt finibus at eros. Cras porta lacus neque, eget efficitur mauris venenatis at. Aenean pellentesque dolor sit amet enim lacinia, quis mollis diam egestas. Fusce mattis, eros vel hendrerit venenatis, orci odio ultrices tellus, sed semper nunc quam sit amet eros. Quisque in ante lobortis, fringilla neque vel, finibus lacus. Nulla eget ante eros. Mauris sodales augue est, in tincidunt risus venenatis non. Nulla non lacinia metus. Aenean molestie placerat enim, id tristique felis auctor id. Sed vitae orci turpis. Donec consectetur odio ac mi vehicula, a consequat augue ornare."
     },
     {
